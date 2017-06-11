3bcefe98365b492316d24f6618392c70e5f46fd9
joon <joonsubtalk@gmail.com>
2017-06-10 18:50:29 -0700

    Async in Redux

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,1 +1,38 @@
-export const CREATE_COURSE = "CREATE_COURSE";
+export const LOAD_COURSES_SUCCESS = "LOAD_COURSES_SUCCESS";
+
+/*
+
+
+
+
+    this.state = {
+      course: {title: ''}
+    };
+    // for performance vs binding in render function
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+
+  onClickSave() {
+    this.props.actions.createCourse(this.state.course);
+  }
+
+  
+        <h2>Add Course</h2>
+
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}
+          />
+
+        <input
+          type="submit"
+          onClick={this.onClickSave}
+          value="Save"
+          />
+*/
